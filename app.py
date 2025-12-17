6801dec52cfc4feb836c7b0ecc0a23a3e60d8a61
ramavathvakil <rathodvakeel59@gmail.com>
2025-12-17 15:04:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+from pathlib import Path
+
+st.set_page_config(
+    page_title="Student Task Dashboard",
+    layout="wide"
+)
+
+html_file = Paath("index.html").read_text(encoding="utf-8")
+
+st.components.v1.html(html_file, height=990, scrolling=True)
+
